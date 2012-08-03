e94591b31f70c5d2a90a343100a471098d44af13
Ben Diedrich <bldiedrich@gmail.com>
2012-08-03 17:29:41 -0400

    starting work on adding genetic algorithm calculation of solar sail trajectories

---
+++ sailgeco.lisp
@@ -0,0 +1,7 @@
+;; Solar sail trajectory optimization via genetic algorithm as implemented in Genetic Evolution through Combination of Objects (GECO)
+
+(asdf:load-system :geco)
+(asdf:load-system :bld-orbit)
+
+(in-package :bld-orbit)
+
